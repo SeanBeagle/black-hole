902138356bb2f85436f6149b0eb19de208dc62af
Sean Beagle <seanbeagle@gmail.com>
2024-08-16 05:33:29 -0400

    all in main

--- main.go
+++ main.go
@@ -117,3 +117,16 @@ func main() {
 	_ = server
 	select {}
 }
+
+var index = `
+<script>
+  const socket = new WebSocket('ws://localhost:8081/get');
+  socket.addEventListener('open', function (event) {
+	socket.send('Hello Server!');
+  });
+  
+  socket.addEventListener('message', function (event) {
+	console.log('Message from server ', event.data);
+});
+</script>
+`
